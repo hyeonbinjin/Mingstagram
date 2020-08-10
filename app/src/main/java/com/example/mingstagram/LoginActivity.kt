f3ffa4eb4b722bac8bcecbe516f216ae6d436c10
MinjiEom <99x04@naver.com>
2020-08-10 23:49:02 +0900

    make google login service & fix error about intent

--- app/src/main/java/com/example/mingstagram/LoginActivity.kt
+++ app/src/main/java/com/example/mingstagram/LoginActivity.kt
@@ -4,12 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
+import com.example.mingstagram.MainActivity as MainActivity
 
  class LoginActivity : AppCompatActivity() {
      var auth : FirebaseAuth? = null
+     var googleSignInClient : GoogleSignInClient? = null
+     var GOOGLE_LOGIN_CODE = 9001
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -17,7 +26,46 @@ import kotlinx.android.synthetic.main.activity_login.*
         email_login_botton.setOnClickListener {
             signinAndSignup()
         }
+        google_sign_in_button.setOnClickListener {
+            //First step
+            googleLogin()
+        }
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
     }
+     fun googleLogin(){
+         var signInIntent = googleSignInClient?.signInIntent
+         startActivityForResult(signInIntent,GOOGLE_LOGIN_CODE)
+     }
+
+     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+         super.onActivityResult(requestCode, resultCode, data)
+         if(requestCode ==GOOGLE_LOGIN_CODE){
+             var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+             if(result!!.isSuccess){
+                 var account = result.signInAccount
+                 // Second step
+                 firebaseAuthWithGoogle(account)
+             }
+         }
+     }
+     fun firebaseAuthWithGoogle(account: GoogleSignInAccount?){
+         var credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+         auth?.signInWithCredential(credential)
+             ?.addOnCompleteListener {
+                     task ->
+                 if(task.isSuccessful){
+                     //Login
+                     moveMainPage(task.result?.user)
+                 } else{
+                     //show the error message
+                     Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+                 }
+             }
+     }
      fun signinAndSignup(){
          auth?.createUserWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())
              ?.addOnCompleteListener {
@@ -25,8 +73,6 @@ import kotlinx.android.synthetic.main.activity_login.*
                 if(task.isSuccessful){
                     //Creating a user account
                     moveMainPage(task.result?.user)
-                }else if(task.exception?.message.isNullOrEmpty()){
-                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
                 }else{
                     //Login if you have account
                     signinEmail()
@@ -48,7 +94,7 @@ import kotlinx.android.synthetic.main.activity_login.*
      }
      fun moveMainPage(user:FirebaseUser?){
          if (user != null){ // 유저가 존재할
-             startActivity(Intent())
+             startActivity(Intent(this,MainActivity::class.java))
          }
      }
 }
